821a90bbaa11cc1cf41bb126fa48a8c1b1a78672
KodjoCode <appohfaiths@gmail.com>
2023-11-25 23:42:50 +0000

    folder setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 4500
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the support desk api'})
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
