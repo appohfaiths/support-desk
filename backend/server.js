ec9c27cc5115f8b6c5564a28da407d97375863c6
KodjoCode <appohfaiths@gmail.com>
2023-11-27 16:58:34 +0000

    complete auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+// const connectDB2 = require('./config/db2')
 const PORT = process.env.PORT || 4500
+
+// connect to database
+connectDB()
+// connectDB2()
 
 const app = express()
 
@@ -13,6 +21,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
 
